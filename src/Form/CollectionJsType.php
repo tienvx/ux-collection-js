60bc2e0dd07caaaa46e95b11bae1018b2df71ea1
tienvx <tien.xuan.vo@gmail.com>
2021-10-16 13:40:14 +0700

    Fix entry options not allowed

--- src/Form/CollectionJsType.php
+++ src/Form/CollectionJsType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CollectionJsType extends AbstractType
@@ -23,18 +24,18 @@ class CollectionJsType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver): void
     {
+        $entryOptionsNormalizer = function (Options $options, $value) {
+            $value['block_prefix'] = 'collection_js_entry';
+
+            return $value;
+        };
+
         $resolver->setDefaults([
             'allow_move_up' => false,
             'allow_move_down' => false,
-            'entry_options' => function (OptionsResolver $entryOptionsResolver) {
-                $entryOptionsResolver
-                    ->setDefaults([
-                        'label' => false,
-                        'block_prefix' => 'collection_js_entry',
-                    ]);
-            },
             'call_post_add_on_init' => false,
         ]);
+        $resolver->setNormalizer('entry_options', $entryOptionsNormalizer);
     }
 
     /**
